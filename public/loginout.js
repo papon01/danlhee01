f59f6c16c5cc4eb757c13834309ed0f0f2a9da89
papon01 <papon0102@gmail.com>
2023-02-05 12:03:11 +0700

    7.4

--- public/loginout.js
+++ public/loginout.js
@@ -18,67 +18,56 @@ if (Object.keys(params).length > 0) {
 // If there's an access token, try an API request.
 // Otherwise, start OAuth 2.0 flow.
 function simpleLogin() {
-  var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
-  if (params && params['access_token']) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET',
-        'https://www.googleapis.com/drive/v3/about?fields=user&' +
-        'access_token=' + params['access_token']);
-    xhr.onreadystatechange = function (e) {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.response);
-
-        //add section by papon
-
-        document.getElementById(simpleLogin()).innerHTML = 'Logout';
-        document.getElementById(simpleLogin()).onclick = oauth2SignIn();
-        document.getElementById('headM').onload = WT();
-
-
-
-
-
-      } else if (xhr.readyState === 4 && xhr.status === 401) {
-        // Token invalid, so prompt for user permission.
-        oauth2SignIn();
-      }
-    };
-    xhr.send(null);
-  } else {
-    oauth2SignIn();
-  }
-}
-
-/*
- * Create form to request access token from Google's OAuth 2.0 server.
- */
-function oauth2SignIn() {
-  // Google's OAuth 2.0 endpoint for requesting an access token
-  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
-
-  // Create element to open OAuth 2.0 endpoint in new window.
-  var form = document.createElement('form');
-  form.setAttribute('method', 'GET'); // Send as a GET request.
-  form.setAttribute('action', oauth2Endpoint);
-
-  // Parameters to pass to OAuth 2.0 endpoint.
-  var params = {'client_id': YOUR_CLIENT_ID,
-                'redirect_uri': YOUR_REDIRECT_URI,
-                'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
-                'state': 'login_S',
-                'include_granted_scopes': 'true',
-                'response_type': 'token'};
-
-  // Add form parameters as hidden input values.
-  for (var p in params) {
-    var input = document.createElement('input');
-    input.setAttribute('type', 'hidden');
-    input.setAttribute('name', p);
-    input.setAttribute('value', params[p]);
-    form.appendChild(input);
+    var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
+    if (params && params['access_token']) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET',
+          'https://www.googleapis.com/drive/v3/about?fields=user&' +
+          'access_token=' + params['access_token']);
+      xhr.onreadystatechange = function (e) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          console.log(xhr.response);
+        } else if (xhr.readyState === 4 && xhr.status === 401) {
+          // Token invalid, so prompt for user permission.
+          oauth2SignIn();
+        }
+      };
+      xhr.send(null);
+    } else {
+      oauth2SignIn();
+    }
   }
 
-  // Add form to page and submit it to open the OAuth 2.0 endpoint.
-  document.body.appendChild(form);
-  form.submit();
+  /*
+   * Create form to request access token from Google's OAuth 2.0 server.
+   */
+  function oauth2SignIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+    // Create element to open OAuth 2.0 endpoint in new window.
+    var form = document.createElement('form');
+    form.setAttribute('method', 'GET'); // Send as a GET request.
+    form.setAttribute('action', oauth2Endpoint);
+
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {'client_id': YOUR_CLIENT_ID,
+                  'redirect_uri': YOUR_REDIRECT_URI,
+                  'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                  'state': 'login_S',
+                  'include_granted_scopes': 'true',
+                  'response_type': 'token'};
+
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'hidden');
+      input.setAttribute('name', p);
+      input.setAttribute('value', params[p]);
+      form.appendChild(input);
+    }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
 }
